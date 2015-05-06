d292e3361875cd4b7e43407c93ac5fcf89aa199c
David McElligott <damc.dev@gmail.com>
2015-05-05 23:11:45 -0500

    added readme and usage info

---
+++ README.md
@@ -0,0 +1,13 @@
+# How to install
+
+git clone https://github.com/damc-dev/media-service-cast-torrent.git
+
+cd media-service-cast-torrent
+
+npm install
+
+# Usage
+
+node app.js
+
+http://localhost:3000/cast/magnet/{url-encoded magnet link}
--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 var express = require('express');
-//var cast = require('castnow');
 var app = express();
 var Player = require('./player');
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 app.get('/cast/magnet/:link', function (req, res, next) {
   var magnetLink = req.params.link
@@ -13,12 +8,12 @@ app.get('/cast/magnet/:link', function (req, res, next) {
   res.send(player.getStatus());
 });
 
-
 var server = app.listen(3000, function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('media-service-cast-torrent listening at http://%s:%s', host, port);
+  console.log('Usage: curl http://%s:%s/cast/magnet/{url-encoded magnet link}', host, port);
 
 });
